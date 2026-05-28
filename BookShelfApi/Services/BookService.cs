94b7c91d8fd8ec2805ece61c50b6de287a937216
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2026-05-28 20:26:40 +0300

    Book service CRUD

---
+++ BookShelfApi/Services/BookService.cs
@@ -0,0 +1,65 @@
+﻿using BookShelfApi.Dtos;
+using BookShelfApi.Models;
+namespace BookShelfApi.Services
+
+{
+    public class BookService
+    {
+        private readonly List<Book> _books = new();
+
+        private int _nextId = 1;
+
+        public List<Book> GetAll() 
+        { 
+            return _books;
+        }
+
+        public Book? GetById(int id)
+        {
+            return _books.FirstOrDefault(book => book.Id == id);
+        }
+
+        public Book Create(CreateBookDTO dto)
+        {
+            var book = new Book
+            {
+                Id = _nextId++,
+                Title = dto.Title,
+                Author = dto.Author,
+                Year = dto.Year,
+                IsRead = false
+            };
+
+            _books.Add(book);
+            return book;
+        }
+
+        public bool MarkAsRead(int id)
+        {
+            var book = GetById(id);
+
+            if (book is null)
+            {
+                return false;
+            }
+
+            book.IsRead = true;
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var book = GetById(id);
+
+            if (book is null)
+            {
+                return false;
+            }
+
+            _books.Remove(book);
+
+            return true;
+        }
+    }
+}
