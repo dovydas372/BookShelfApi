e65f000aace2fe36046577f499454f3259940753
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2026-05-28 18:43:22 +0300

    created bookDTO

--- BookShelfApi/Dtos/CreateBookDTO.cs
+++ BookShelfApi/Dtos/CreateBookDTO.cs
@@ -1,12 +1,9 @@
-﻿namespace BookShelfApi.Models
+﻿namespace BookShelfApi.Dtos
 {
-    public class Book
+    public class CreateBookDTO
     {
-        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public int Year { get; set; }
-
-        public bool IsRead { get; set; }
     }
 }
